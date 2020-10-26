11f5cfd37f843807cc69b7dd2013cff64a3850de
thesecretnix <59189977+thesecretnix@users.noreply.github.com>
2020-10-26 18:49:08 +0530

    Basic structure done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ResizableBox } from 'react-resizable';
+import style from './App.css';
+
+export default class App extends Component {
+
+    state = {
+        width: 200,
+        height: 200,
+        absoluteWidth: 200,
+        absoluteHeight: 200,
+        absoluteLeft: 0,
+        absoluteTop: 0,
+    };
+
+    onResetClick = () => {
+        this.setState({ width: 200, height: 200, absoluteWidth: 200, absoluteHeight: 200 });
+    };
+
+    onResize = (event, { element, size, handle }) => {
+        this.setState({ width: size.width, height: size.height });
+    };
+
+    onResizeAbsolute = (event, { element, size, handle }) => {
+        this.setState((state) => {
+            let newLeft = state.absoluteLeft;
+            let newTop = state.absoluteTop;
+            const deltaHeight = size.height - state.absoluteHeight;
+            const deltaWidth = size.width - state.absoluteWidth;
+            if (handle[0] === 'n') {
+                newTop -= deltaHeight;
+            } else if (handle[0] === 's') {
+                newTop += deltaHeight;
+            }
+            if (handle[handle.length - 1] === 'w') {
+                newLeft -= deltaWidth;
+            } else if (handle[handle.length - 1] === 'e') {
+                newLeft += deltaWidth;
+            }
+
+            return {
+                absoluteWidth: size.width,
+                absoluteHeight: size.height,
+                absoluteLeft: newLeft,
+                absoluteTop: newTop,
+            };
+        });
+    };
+
+
+    render() {
+
+        return (
+            <div className='container'>
+
+                <div style={{ overflow: 'scroll', justifyContent: 'center', alignItems: 'center' }}>
+
+                    <div className="layoutRoot">
+                        <ResizableBox
+                            className="custom-box box"
+                            width={window.innerWidth * 0.3}
+                            height={300}
+                            handle={(h) => <span className={`custom-handle custom-handle-${h}`} />}
+                            handleSize={[8, 8]}
+                            resizeHandles={['sw', 'se', 'nw', 'ne', 'w', 'e', 'n', 's']}>
+                            <span className="text">{"<ResizableBox> with custom handles in all locations."}</span>
+                        </ResizableBox>
+                        <ResizableBox
+                            className="custom-box box"
+                            width={window.innerWidth * 0.63}
+                            height={300}
+                            handle={(h) => <span className={`custom-handle custom-handle-${h}`} />}
+                            handleSize={[8, 8]}
+                            resizeHandles={['sw', 'se', 'nw', 'ne', 'w', 'e', 'n', 's']}>
+                            <span className="text">{"<ResizableBox> with custom handles in all locations."}</span>
+                        </ResizableBox>
+                    </div>
+
+                    <div className="layoutRoot">
+                        <ResizableBox
+                            className="custom-box box"
+                            width={window.innerWidth}
+                            height={300}
+                            handle={(h) => <span className={`custom-handle custom-handle-${h}`} />}
+                            handleSize={[8, 8]}
+                            resizeHandles={['sw', 'se', 'nw', 'ne', 'w', 'e', 'n', 's']}>
+                            <span className="text">{"<ResizableBox> with custom handles in all locations."}</span>
+                        </ResizableBox>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
 }
-
-export default App;
